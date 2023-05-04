a125d4165c109ead67e068f06857f1d5ba936273
AldonIsenberg11 <aldon.isenberg2@gmail.com>
2023-05-04 09:46:13 -0500

    logging users in

--- util/auth.js
+++ util/auth.js
@@ -2,11 +2,22 @@ import axios from 'axios'
 
 const API_KEY = 'AIzaSyC2HuA3L0cl97KRwcBeyBeE_9KAfRL4P9A'
 
-export async function createUser(email, password) {
-    const response = await axios.post(
-        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
-        { email, password, returnSecureToken: true }
-    )
+export async function authenticate(mode, email, password) {
+    const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`
+
+    const response = await axios.post(url, {
+        email,
+        password,
+        returnSecureToken: true 
+    })
 
     console.log(response.data)
+}
+
+export async function createUser(email, password) {
+    await authenticate('signUp', email, password)
+}
+
+export async function login(email, password) {
+    return authenticate('signInWithPassword', email, password)
 }
