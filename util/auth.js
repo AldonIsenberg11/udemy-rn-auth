33da00ee302d709c57894171a52f10702643b7fb
AldonIsenberg11 <aldon.isenberg2@gmail.com>
2023-05-04 09:37:15 -0500

    creating new user

--- util/auth.js
+++ util/auth.js
@@ -1,10 +1,12 @@
 import axios from 'axios'
 
-const API_KEY = AIzaSyC2HuA3L0cl97KRwcBeyBeE_9KAfRL4P9A
+const API_KEY = 'AIzaSyC2HuA3L0cl97KRwcBeyBeE_9KAfRL4P9A'
 
-async function createUser(email, password) {
+export async function createUser(email, password) {
     const response = await axios.post(
-        `https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=${API_KEY}`,
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
         { email, password, returnSecureToken: true }
     )
+
+    console.log(response.data)
 }
